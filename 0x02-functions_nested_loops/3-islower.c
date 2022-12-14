979a3b75a294fdbbd88271ae9590695ea12b5f55
Chigozie Vivian Onwumere <chigozievivianonwumere9@gmail.com>
2022-12-14 04:37:33 -0800

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+
+#include "main.h"
+/**
+ * _islower - checks for lowercase character
+ * @c: the chracter check
+ * Return: 1 if c is lowercase, 0 if otherwise
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
+
