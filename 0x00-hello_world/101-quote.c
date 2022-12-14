97c62dc40003a3bd7ab8b6c694ba51f1a299607d
Chigozie Vivian Onwumere <chigozievivianonwumere9@gmail.com>
2022-12-13 16:53:20 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,14 +2,12 @@
 #include <unistd.h>
 
 /**
-
  * main - Entry point
  *
  * Description: prints the useful art
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
