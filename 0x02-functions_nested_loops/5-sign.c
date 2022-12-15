47dab02cfd84abac52ed39a8d0b474c172736525
Chigozie Vivian Onwumere <chigozievivianonwumere9@gmail.com>
2022-12-14 22:37:06 -0800

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,24 +1,27 @@
 #include "main.h"
 /**
- * print_sign - Entry point
- * @n: checks integer n
- * Return: Success
+ * print_sign-print sign of a number
+ * @n: character to be verified
+ * Return: return 0 or 1
  */
 int print_sign(int n)
 {
-	if (n > 0)
+	if (n == 0)
 	{
-		_putchar('+');
-		return (1);
-		else if (n == 0)
+		_putchar('0');
+
+		return (0);
+	}
+		else if (n > 0)
 		{
-			_putchar(48);
-			return (0);
+			_putchar('+');
+
+			return (1);
 		}
 		else
 		{
 			_putchar('-');
+
 			return (-1);
 		}
-	}
 }
