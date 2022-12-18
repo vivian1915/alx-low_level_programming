c54c3a817c4e9184cd0d174dc276de555e7ba3a5
Chigozie Vivian Onwumere <chigozievivianonwumere9@gmail.com>
2022-12-18 01:11:05 +0100

    task 2

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,17 +12,24 @@ int largest_number(int a, int b, int c)
 {
     int largest;
 
-    if (a > b && b > c)
+    if (a > b)
     {
-        largest = a;
+	    if (a > c)
+	    {
+		    largest = a;
+	    }
+	    else
+	    {
+		    largest = c;
+	    }
     }
-    else if (b > a && a > c)
+    else if (b > c)
     {
         largest = b;
     }
     else
     {
-        largest = c;
+	    largest = c;
     }
 
     return (largest);
