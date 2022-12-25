8eafab03adb5ef8eac75d6a35f68054a00bf58a3
Chigozie Vivian Onwumere <chigozievivianonwumere9@gmail.com>
2022-12-25 23:48:00 +0100

    task 5

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+*reverse_array - function that reverse content of array
+*@a: array
+*@n: number of elements
+*/
+
+void reverse_array(int *a, int n)
+{
+	int i = 0;
+	int j = 0;
+	int *init, *final;
+	int aux = 0;
+
+	init = a;
+	final = a;
+	n -= 1;
+	while (i < n)
+		i += 1;
+	while (j <= i)
+	{
+		aux = *(init + j);
+		*(init + j) = *(final + i);
+		*(final + i) = aux;
+		j++;
+		i--;
+	}
+}
