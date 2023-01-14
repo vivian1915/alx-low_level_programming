c9a3ec313dac4863adce8dea1221eaf5e53b7617
Chigozie Vivian Onwumere <chigozievivianonwumere9@gmail.com>
2023-01-14 08:04:52 +0100

    h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,6 @@
-#ifndef FILE_MAIN
-#define FILE_MAN
+#ifndef MAIN_H
+#define MAIN_H
+#include <stddef.h>
 
 int _putchar(char c);
 int _islower(int c);
@@ -21,5 +22,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-
 #endif
